0d0d7f81d3d2cf87958e9696d367cac92001749d
phii <97967090+ivialvam@users.noreply.github.com>
2024-06-14 10:02:09 +0700

    Delete flutter_counter_cubit directory

--- flutter_counter_cubit/android/app/src/main/kotlin/com/example/flutter_counter_cubit/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_counter_cubit
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- flutter_counter_cubit/lib/counter/app.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_counter_cubit/counter/counter.dart';
-import 'package:flutter_counter_cubit/counter/view/counter_page.dart';
-
-class CounterApp extends MaterialApp {
-  const CounterApp({super.key}) : super(home: const CounterPage());
-}
